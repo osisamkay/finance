921fceb42764a75e5bdf3680064bfb53cbbe93ff
osisamkay <osisami.oj@gmail.com>
2024-06-19 20:51:15 -0600

    completed playground

--- app/playground/page.jsx
+++ app/playground/page.jsx
@@ -1,5 +1,8 @@
 import Button from "../../components/button";
+import Input from "../../components/input";
+import Label from "../../components/label";
 import PageHeader from "../../components/page-header";
+import Select from "../../components/select";
 import TransactionItem from "../../components/transaction-item";
 import TransactionSummaryItem from "../../components/transaction-summary-item";
 import Trend from "../../components/trend";
@@ -87,6 +90,32 @@ export default function Page(params) {
           <Button size="xs">Hello</Button>
           <Button size="sm">Hello</Button>
           <Button size="lg">Hello</Button>
+        </div>
+      </div>
+      <div>
+        <h2 className="mb-4 text-lg font-mono">Forms</h2>
+        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <Label className="mb-1">Your name</Label>
+            <Input type="text" placeholder="Type something in here..." />
+          </div>
+
+          <div>
+            <Label className="mb-1">City</Label>
+            <Select>
+              <option>Warsaw</option>
+              <option>Berlin</option>
+              <option>London</option>
+            </Select>
+          </div>
+
+          <div className="flex items-center">
+            <Input type="checkbox" id="terms" />
+            <Label className="ml-2" htmlFor="terms">
+              Accept terms
+            </Label>
+          </div>
         </div>
       </div>
     </main>
