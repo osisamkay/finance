ce68f0052ecd84d16e1c9db562b554ef8b116d30
osisamkay <osisami.oj@gmail.com>
2024-06-22 15:39:04 -0600

    feat:added add transaction page/form

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,9 @@
 import { Suspense } from "react";
 
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
+
+import { variants, sizes } from "../../lib/variants";
 import TransactionList from "./components/transaction-list";
 import TransactionListFallback from "./components/transaction-list-fallback";
 import Trend from "./components/trend";
@@ -8,6 +12,9 @@ import TrendFallback from "./components/trend-fallback";
 export default function Page() {
   return (
     <>
+      <section>
+        <div className="text-4xl">Summary</div>
+      </section>
       <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
         <Suspense fallback={<TrendFallback />}>
           <Trend type="income" />
@@ -21,6 +28,16 @@ export default function Page() {
         <Suspense fallback={<TrendFallback />}>
           <Trend type="investment" />
         </Suspense>
+      </section>
+      <section className="flex justify-between item-center mb-8">
+        <h2 className="text-2xl">Transactions</h2>
+        <Link
+          className={`flex items-center space-x-1 ${variants["outline"]} ${sizes["sm"]}`}
+          href="/dashboard/transactions/add"
+        >
+          <PlusCircle className="w-d h-4" />
+          <div className="add">Add</div>
+        </Link>
       </section>
       <Suspense fallback={<TransactionListFallback />}>
         <TransactionList />
---
+++ lib/consts.jsx
@@ -0,0 +1,9 @@
+export const types = ["income", "expense", "investment", "saving"];
+export const categories = [
+  "Housing",
+  "Transport",
+  "Health",
+  "Food",
+  "Education",
+  "Other",
+];
